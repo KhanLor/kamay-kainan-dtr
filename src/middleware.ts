d6a091341419837ebc861600ac8e95d0780f665d
KhanLor <khanyao_lor@sjp2cd.edu.ph>
2026-04-21 11:01:01 +0800

    added new file

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,13 @@ export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request,
   });
+  const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseKey!,
     {
       cookies: {
         getAll() {
